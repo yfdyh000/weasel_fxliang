ba6809613885ad808f82a7aabb529e3ec4d1e568
liangfx <fx.liang@outlook.com>
2022-08-25 20:25:22 +0800

    change to vs2019

---
+++ env.vs2019_ci.bat
@@ -0,0 +1,13 @@
+rem Customize your build environment and save the modified copy to env.bat
+set WEASEL_ROOT=%CD%
+set BOOST_ROOT=%WEASEL_ROOT%\deps\boost_1_78_0
+
+rem REQUIRED: path to Boost source directory
+set BOOST_ROOT=C:\Libraries\boost_1_76_0
+
+rem OPTIONAL: Visual Studio version and platform toolset
+set BJAM_TOOLSET=msvc-14.2
+set PLATFORM_TOOLSET=v142
+
+rem OPTIONAL: path to additional build tools
+rem set DEVTOOLS_PATH=%ProgramFiles%\Git\cmd;%ProgramFiles%\Git\usr\bin;%ProgramFiles%\CMake\bin;C:\Python27;
